31264b08c503da31a4ad7c918d686ca02cb5972e
Mark Ditzel <mark.ditzel@ed.ac.uk>
2018-09-21 15:22:58 +0100

    added catch to CREATE route

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -1,68 +1,71 @@
 const express = require('express');
 const ObjectID = require('mongodb').ObjectID;
 
-const createRouter = function(collection){
+const createRouter = function (collection) {
     const router = express.Router();
 
     // INDEX
     router.get('/', (req, res) => {
         collection
-        .find()
-        .toArray()
-        .then((docs)=>{
-            res.json(docs)
-        })
-        .catch((error) => {
-            console.error(error);
-        });
+            .find()
+            .toArray()
+            .then((docs) => {
+                res.json(docs)
+            })
+            .catch((error) => {
+                console.error(error);
+            });
     })
 
     // SHOW
     router.get('/:id', (req, res) => {
         const id = req.params.id;
         collection
-        .find({ _id: ObjectID(id)})
-        .toArray()
-        .then((doc) => {
-            res.json(doc)
-        })
-        .catch((error) => {
-            console.error(error)
-        });
+            .find({ _id: ObjectID(id) })
+            .toArray()
+            .then((doc) => {
+                res.json(doc)
+            })
+            .catch((error) => {
+                console.error(error)
+            });
     })
 
     // CREATE
-    router.post('/', (req, res) =>{
+    router.post('/', (req, res) => {
         const newData = req.body;
         collection
-        .insertOne(newData)
-        .then(() => 
-        collection
-        .find()
-        .toArray())
-        .then((docs) =>
-        res.json(docs));
-    });
+            .insertOne(newData)
+            .then(() =>
+                collection
+                    .find()
+                    .toArray())
+            .then((docs) =>
+                res.json(docs));
+    })
+        .catch((error) => {
+            console.error(error)
+        });
 
     // UPDATE
     router.put('/:id', (req, res) => {
         const id = req.params.id;
         const updatingItem = req.body;
         collection.updateOne(
-            {_id: ObjectID(id)},
-            {$set: updatingItem}
+            { _id: ObjectID(id) },
+            { $set: updatingItem }
         )
-        .then(() => {
-            collection
-            .find()
-            .toArray()
-            .then((docs) => {
-                res.json(docs)
+            .then(() => {
+                collection
+                    .find()
+                    .toArray()
+                    .then((docs) => {
+                        res.json(docs)
+                    })
             })
-        })
-        .catch((error) => {
-            console.error(error)
-        })
+            .catch((error) => {
+                console.error(error)
+            })
     })
 
     // DELETE
