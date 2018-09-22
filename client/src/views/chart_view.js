18daeff62111bd56a9e5da3c1cb364eff792f8b5
Tahnee Downie <tahneedownie@yahoo.com>
2018-09-22 17:43:26 +0100

    Added more test data and changed to type bar.

--- client/src/views/chart_view.js
+++ client/src/views/chart_view.js
@@ -5,12 +5,16 @@ const ChartView = function(container){
     this.testData = {
         protein: "50",
         calcium: "15",
-        vitamin_c: "35"
+        vitamin_c: "35",
+        fibre: "57",
+        iron: "10",
+        zinc: "6",
+        potassium: "2"
     }
 }
 
 ChartView.prototype.render = function(){
-    this.createTableElements(); 
+    this.createTableElements();
     this.insertDataIntoTable();
     this.displayChart();
 }
@@ -24,19 +28,35 @@ ChartView.prototype.createTableElements = function(){
     this.row2 = this.body.insertRow(0);
     this.row3 = this.body.insertRow(1);
     this.row4 = this.body.insertRow(2);
+    this.row5 = this.body.insertRow(3);
+    this.row6 = this.body.insertRow(4);
+    this.row7 = this.body.insertRow(5);
+    this.row8 = this.body.insertRow(6);
     this.thTitle = document.createElement('th');
     this.thTitle2 = document.createElement('th');
     this.th1 = document.createElement('th');
     this.th2 = document.createElement('th');
     this.th3 = document.createElement('th');
+    this.th4 = document.createElement('th');
+    this.th5 = document.createElement('th');
+    this.th6 = document.createElement('th');
+    this.th7 = document.createElement('th');
     this.row1.appendChild(this.thTitle);
     this.row1.appendChild(this.thTitle2);
     this.row2.appendChild(this.th1);
     this.row3.appendChild(this.th2);
     this.row4.appendChild(this.th3);
+    this.row5.appendChild(this.th4);
+    this.row6.appendChild(this.th5);
+    this.row7.appendChild(this.th6);
+    this.row8.appendChild(this.th7);
     this.cell1 = this.row2.insertCell(1);
     this.cell2 = this.row3.insertCell(1);
     this.cell3 = this.row4.insertCell(1);
+    this.cell4 = this.row5.insertCell(1);
+    this.cell5 = this.row6.insertCell(1);
+    this.cell6 = this.row7.insertCell(1);
+    this.cell7 = this.row8.insertCell(1);
     this.container.appendChild(this.dataTable);
 }
 
@@ -47,8 +67,16 @@ ChartView.prototype.insertDataIntoTable = function(){
     this.cell1.innerHTML = this.testData.protein;
     this.th2.textContent = "Calcium";
     this.cell2.innerHTML = this.testData.calcium;
-    this.th3.textContent = "vitamin C";
+    this.th3.textContent = "Vitamin C";
     this.cell3.innerHTML = this.testData.vitamin_c;
+    this.th4.textContent = "Fibre";
+    this.cell4.innerHTML = this.testData.fibre;
+    this.th5.textContent = "Iron";
+    this.cell5.innerHTML = this.testData.iron;
+    this.th6.textContent = "Zinc";
+    this.cell6.innerHTML = this.testData.zinc;
+    this.th7.textContent = "Potassium";
+    this.cell7.innerHTML = this.testData.potassium;
 }
 
 ChartView.prototype.displayChart = function(){
@@ -58,7 +86,7 @@ ChartView.prototype.displayChart = function(){
             enablePolling: true
         },
         chart: {
-            type: 'column'
+            type: 'bar'
         },
         title: {
             text: 'Your total daily intake'
