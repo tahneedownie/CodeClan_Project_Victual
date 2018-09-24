4783d8ccdf33abb2631748918b31160a5b7303dd
Tahnee Downie <tahneedownie@yahoo.com>
2018-09-24 11:32:22 +0100

    Refactored chart data into for loop and removed old code.

---
+++ client/src/helpers/recipe_request.js
@@ -0,0 +1,56 @@
+const dotenv = require('dotenv');
+dotenv.load();
+const apiKey = "3aef42721e55d7947be68804a84435d9";//process.env.RECIPE_API_KEY;
+const applicationID = "3f9b33ea";//process.env.RECIPE_APPLICATION_ID;
+const NutrientRDA = require('../models/nutrient_RDA.js');
+const RecipeRequest = function () {
+    this.url = `https://api.edamam.com/search?q=&app_id=${applicationID}&app_key=${apiKey}`;
+};
+RecipeRequest.prototype.get = function (deficientVitaminArray, healthRequirement, exclusionsArray) {
+    return fetch(`${this.buildQueryString(deficientVitaminArray, healthRequirement, exclusionsArray)}`)
+        .then((response) => response.json());
+};
+RecipeRequest.prototype.buildQueryString = function(deficientVitaminArray, healthRequirement, exclusionsArray){
+    let query = "";
+    query += this.url;
+    query += this.getNutrientQuery(deficientVitaminArray);
+    query += this.getNumberOfResultsQuery();
+    query += this.getHealthRequirementQuery(healthRequirement);
+    query += this.getExclusionsQuery(exclusionsArray);
+    console.log(query);
+    return query;
+}
+RecipeRequest.prototype.getNumberOfResultsQuery = function(){
+    return "from=0&to=20";
+}
+RecipeRequest.prototype.getHealthRequirementQuery = function(healthRequirement){
+    if(healthRequirement !== "none"){
+        return `&health=${healthRequirement}`;
+    }
+    else{
+        return "";
+    }
+}
+RecipeRequest.prototype.getExclusionsQuery = function(exclusionsArray){
+    let exclusionsQuery = "";
+    if(exclusionsArray.length !== 0){
+        exclusionsArray.forEach((requirement)=>{
+             exclusionsQuery += `&excluded=${requirement}`;
+        });
+    }
+    return exclusionsQuery;
+}
+RecipeRequest.prototype.getNutrientQuery = function(deficientVitaminArray){
+    let nutrientQuery = "&nutrients";
+    for(let vitamin of deficientVitaminArray){
+        nutrientQuery += `%5B${this.getShortHand(vitamin)}%5D=${this.getMinimumContent(vitamin)}%2B&`
+    };
+    return nutrientQuery;
+}
+RecipeRequest.prototype.getShortHand = function(vitamin){
+    return NutrientRDA.nutrients[vitamin].shortHand;
+}
+RecipeRequest.prototype.getMinimumContent = function(vitamin){
+    return 0.5 * NutrientRDA.nutrients[vitamin].RDA;
+}
+module.exports = RecipeRequest;
--- client/src/views/chart_view.js
+++ client/src/views/chart_view.js
@@ -34,13 +34,17 @@ const ChartView = function(container){
         vitamin_b: {
             name: "Vitamin B",
             amount: 33
+        },
+        vitamin_d: {
+            name: "Vitamin D",
+            amount: 54
         }
+
     }
 }
 
 ChartView.prototype.render = function(){
     this.createTableElements();
-    //this.insertDataIntoTable();
     this.displayChart();
 }
 
@@ -73,24 +77,6 @@ ChartView.prototype.createRows = function(){
     }
 }
 
-// ChartView.prototype.insertDataIntoTable = function(){
-    // this.thTitle.textContent = "% of RDA (Recommended Dietry Allowance)";
-    // this.thTitle2.textContent = "% of RDA (Recommended Dietry Allowance)";
-//     this.th1.textContent = "Protein";
-//     this.cell1.innerHTML = this.testData.protein;
-//     this.th2.textContent = "Calcium";
-//     this.cell2.innerHTML = this.testData.calcium;
-//     this.th3.textContent = "Vitamin C";
-//     this.cell3.innerHTML = this.testData.vitamin_c;
-//     this.th4.textContent = "Fibre";
-//     this.cell4.innerHTML = this.testData.fibre;
-//     this.th5.textContent = "Iron";
-//     this.cell5.innerHTML = this.testData.iron;
-//     this.th6.textContent = "Zinc";
-//     this.cell6.innerHTML = this.testData.zinc;
-//     this.th7.textContent = "Potassium";
-//     this.cell7.innerHTML = this.testData.potassium;
-// }
 
 ChartView.prototype.displayChart = function(){
     Highcharts.chart('chart-container', {
@@ -120,104 +106,3 @@ ChartView.prototype.displayChart = function(){
 }
 
 module.exports = ChartView;
-
-
-
-
-
-
-// const PubSub = require('../helpers/pub_sub.js');
-
-// const ChartView = function(container){
-//     this.container = container;
-//     this.testData = {
-//         protein: {
-//             name: "Protein",
-//             amount: 50
-//         },
-//         calcium: {
-//             name: "Calcium",
-//             amount: 100
-//         },
-//         iron: {
-//             name: "Iron",
-//             amount: 20
-//         },
-//         magnesium: {
-//             name: "Magnesium",
-//             amount: 10
-//         },
-//         fat: {
-//             name: "Fat",
-//             amount: 60
-//         }
-//     }
-// }
-
-// ChartView.prototype.render = function(){
-//     this.createTableElements(); 
-//     this.displayChart();
-// }
-
-// ChartView.prototype.createTableElements = function(){
-//     this.dataTable = document.createElement('table');
-//     this.dataTable.setAttribute('id', 'datatable');
-//     this.header = this.dataTable.createTHead();
-//     this.body = this.dataTable.createTBody();
-//     this.row0 = this.header.insertRow(0);
-//     this.thTitle = document.createElement('th');
-//     this.thTitle2 = document.createElement('th');
-//     this.row0.appendChild(this.thTitle);
-//     this.row0.appendChild(this.thTitle2);
-//     let i = 0;
-//     for(let element in this.testData){
-//         const row = this.body.insertRow(i);
-//         const th = document.createElement('th');
-//         row.appendChild(th);
-//         const cell = row.insertCell(1);
-//         th.textContent = this.testData[element].name;
-//         cell.innerHTML = this.testData[element].amount;
-//         i++;
-//     }
-//     this.container.appendChild(this.dataTable);
-// }
-
-// ChartView.prototype.insertDataIntoTable = function(){
-//     this.thTitle.textContent = "Percentage of your RDA";
-//     this.thTitle2.textContent = "Percentage of your RDA";
-//     this.th1.textContent = "Protein";
-//     this.cell1.innerHTML = this.testData.protein;
-//     this.th2.textContent = "Calcium";
-//     this.cell2.innerHTML = this.testData.calcium;
-//     this.th3.textContent = "vitamin C";
-//     this.cell3.innerHTML = this.testData.vitamin_c;
-// }
-
-// ChartView.prototype.displayChart = function(){
-//     Highcharts.chart('chart-container', {
-//         data: {
-//             table: 'datatable',
-//             enablePolling: true
-//         },
-//         chart: {
-//             type: 'column'
-//         },
-//         title: {
-//             text: 'Your total daily intake'
-//         },
-//         yAxis: {
-//             allowDecimals: false,
-//             title: {
-//                 text: 'Percentage of your RDA'
-//             }
-//         },
-//         tooltip: {
-//             formatter: function () {
-//                 return '<b>' + this.series.name + '</b><br/>' +
-//                     this.point.y + ' ' + this.point.name.toLowerCase();
-//             }
-//         }
-//       });
-// }
-
-// module.exports = ChartView;
