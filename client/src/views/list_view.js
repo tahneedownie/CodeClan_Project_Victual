10318a41f6e7b3eb4be69580a3d2dab4198dc034
Robbie Dumbrell <robert.dumbrell@outlook.com>
2018-09-26 11:11:59 +0100

    list view refactored further

--- client/src/views/list_view.js
+++ client/src/views/list_view.js
@@ -22,6 +22,30 @@ ListView.prototype.renderAll = function (allData) {
     });
 }
 
+ListView.prototype.renderOne = function (dataItem) {
+
+    const div = document.createElement('div');
+    div.className = "food-item";
+
+    const nameHeading = this.createHeading(dataItem);
+    div.appendChild(nameHeading);
+
+    const deleteButton = this.createDeleteButton(dataItem);
+    div.appendChild(deleteButton);
+
+    const expandButton = this.createExpandButton();
+    div.appendChild(expandButton);
+
+    const nutrientList = this.createUnorderedList();
+    this.generateNutrientDetails(nutrientList, dataItem);
+    div.appendChild(nutrientList);
+
+    this.makeDetailsExpandable(expandButton, nutrientList);
+
+    this.listContainer.appendChild(div);
+
+}
+
 ListView.prototype.createHeading = function (dataItem) {
     const nameHeading = document.createElement('h3');
     nameHeading.className = "food-item-heading"
@@ -43,8 +67,17 @@ ListView.prototype.createExpandButton = function () {
     const expandButton = document.createElement('button');
     expandButton.className = "item-expand-button"
     expandButton.textContent = 'RDA %s';
-    
     return expandButton;
+}
+
+ListView.prototype.makeDetailsExpandable = function (button, unorderedList) {
+    button.addEventListener('click', () => {
+        if (unorderedList.style.display === 'none') {
+            unorderedList.style.display = 'block';
+        } else if (unorderedList.style.display === 'block') {
+            unorderedList.style.display = 'none';
+        }
+    })
 }
 
 ListView.prototype.createUnorderedList = function () {
@@ -75,69 +108,9 @@ ListView.prototype.generateNutrientDetails = function (unorderedList, dataItem) 
     })
 }
 
-ListView.prototype.renderOne = function (dataItem) {
-
-    // const amountOfUnits = parseFloat(dataItem.amount);
-
-    const div = document.createElement('div');
-    div.className = "food-item";
-
-    const nameHeading = this.createHeading(dataItem);
-    // const nameHeading = document.createElement('h3');
-    // nameHeading.className = "food-item-heading"
-    // nameHeading.textContent = `${dataItem.name}: ${dataItem.amount} ${dataItem.measurement}`;
-    div.appendChild(nameHeading);
-
-    const deleteButton = this.createDeleteButton(dataItem);
-    // const deleteButton = document.createElement('button');
-    // deleteButton.className = "item-delete-button";
-    // deleteButton.textContent = 'Delete';
-    // deleteButton.addEventListener('click', () => {
-    //   PubSub.publish('ListView:delete-item-clicked', dataItem._id)
-    // })
-    div.appendChild(deleteButton);
-
-    const expandButton = this.createExpandButton();
-    // const expandButton = document.createElement('button');
-    // expandButton.className = "item-expand-button"
-    // expandButton.textContent = 'RDA %s';
-    div.appendChild(expandButton);
 
 
-    const unorderedList = this.createUnorderedList();
-    // const unorderedList = document.createElement('ul');
-    // unorderedList.className = "item-details-list";
-    // unorderedList.style.display = 'none';
 
-    // const listItemTextArray = [];
-
-    // for(let nutritionDetailKey of Object.keys(dataItem.details)){
-    //    const nutrientObject = dataItem.details[nutritionDetailKey];
-    //    const totalPercentage = (amountOfUnits * parseFloat(nutrientObject.quantity));
-    //    const listItemText = `${nutrientObject.label}: ${totalPercentage.toFixed(2)}%`;
-    //    listItemTextArray.push(listItemText);
-    // }
-
-    // listItemTextArray.sort();
-    // listItemTextArray.forEach((listItemText)=>{
-    //     const listItem = document.createElement('li');
-    //     listItem.textContent = listItemText;
-    //     unorderedList.appendChild(listItem);
-    // })
-
-    this.generateNutrientDetails(unorderedList, dataItem);
-
-    div.appendChild(unorderedList);
-    this.listContainer.appendChild(div);
-
-    expandButton.addEventListener('click', () => {
-        if (unorderedList.style.display === 'none') {
-            unorderedList.style.display = 'block';
-        } else if (unorderedList.style.display === 'block') {
-            unorderedList.style.display = 'none';
-        }
-    })
-}
 
 
 
